399493890a09fe474cd760785243f984597102ba
flar <flarmn@gmail.com>
2023-07-06 18:31:55 +0300

    Plugin to show error message under object with specified class

---
+++ js/errorMessanger.js
@@ -0,0 +1,31 @@
+﻿/*
+Usage: 
+showErrorMessage(objClass, errorMessage);
+*/
+
+$(document).ready(function () {
+    addErrMessangerStyles();
+});
+
+
+function addErrMessangerStyles(){
+    // Setting CSS styles in JS
+    var styles = `
+    .errMsg{
+        color: red;
+        font-size: 14px;
+        font-weight: "Arial";
+        margin-top: 5px;
+        margin-bottom: 5px;
+    }
+    `
+    // Applying CSS styles to document NODE
+    var styleSheet = document.createElement("style")
+    styleSheet.innerText = styles
+    document.head.appendChild(styleSheet);
+}
+
+
+function showErrorMessage(objClass, errorMessage){
+    $(`.${objClass}`).after(`<div class = "errMsg">${errorMessage}</div>`)
+}
